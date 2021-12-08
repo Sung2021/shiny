a214ab61f0f42b11e9101bd841dacb78462503cf
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2021-12-08 10:53:47 -0500

    Add files via upload

---
+++ Shiny_tumor.gene.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(shiny)
+
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Check the expression of the gene"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("thegene", 'gene',
+                  choices=rownames(df.tmp),
+                  selected  = "Tcf7"),
+      selectInput('thedata','data',
+                  choices = c('tumor'))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  # library(breastCancerNKI)
+  chronic <- read.csv('~/Desktop/shiny/NIK_chronic.csv', row.names = 1)
+  tumor <- read.csv('~/Desktop/shiny/NIK_tumor.csv', row.names = 1)
+  library(dplyr)
+  library(ggplot2)
+  output$plot <- renderPlot({
+    
+    gene <- input$thegene
+    df.tmp <- tumor
+    gene.exp.func <- function(gene,input.data=df.tmp){
+      gene <- as.character(gene)
+      if(gene %in% rownames(input.data)){
+        df <- input.data[gene,] %>% melt() %>% data.frame()
+        df$condition <- substr(df$variable,1,5)
+        df2 <- df %>% group_by(condition) %>% summarize(avg=mean(value), sd=sd(value))
+        p <- ggplot(df2, aes(condition,avg)) + geom_col()+ geom_errorbar(aes(ymin=avg-sd, ymax=avg+sd)) + 
+          ylab(gene) + theme_classic()
+        print(p)
+      }
+      else{
+        message('no gene in this dataset')
+      }
+    }
+    gene.exp.func(gene)
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
